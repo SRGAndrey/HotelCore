17799f19bdb240cc3d0eda1fe4840dc6b1dafcb8
Andrey Ramos <Andreysrg16@hotmail.com>
2018-04-06 17:26:53 -0600

    Commit inicial

---
+++ HotelCore.dominio/IRepositorio/IRepositorioHotel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelCore.dominio.entidades.Objetos;
+
+    public interface IRepositorioHotel
+    {
+    bool insertar(Hotel hotel);
+    Hotel obtenerHotel(String nombre);
+    bool actualizar(Hotel hotel);
+    bool eliminar(String nombre);
+
+    List<Hotel> obtenerHoteles();
+}
+
---
+++ HotelCore.infraestructura.datos/Repositorio/RepositorioHotel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelCore.dominio.entidades.Objetos;
+using HotelCore.infraestructura.datos.Modelo;
+
+public class RepositorioHotel : IRepositorioHotel
+{
+    private HotelDBEntities db = new HotelDBEntities();
+
+    public bool actualizar(Hotel hotel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool eliminar(string nombre)
+    {
+        Hotel hotel = db.Hotel.Find(nombre);
+        db.Hotel.Remove(hotel);
+        return true;
+    }
+
+    public bool insertar(Hotel hotel)
+    {
+        throw new NotImplementedException();
+    }
+
+      public Hotel obtenerHotel(string nombre)
+    {
+        Hotel hotel = db.Hotel.Find(nombre);
+        return hotel;
+    }
+
+    public List<Hotel> obtenerHoteles()
+    {
+        List<Hotel> hoteles = db.Hotel.ToList();
+        return hoteles;
+    }
+
+    Hotel IRepositorioHotel.obtenerHotel(string nombre)
+    {
+        throw new NotImplementedException();
+    }
+
+    List<Hotel> IRepositorioHotel.obtenerHoteles()
+    {
+        throw new NotImplementedException();
+    }
+}
+
