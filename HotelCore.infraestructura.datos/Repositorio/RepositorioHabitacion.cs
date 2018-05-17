498cb1fbf5d999c52539a9937c2ee846be26f140
Andrey Ramos <Andreysrg16@hotmail.com>
2018-05-16 18:36:57 -0600

    Se agrego todo lo de habitacion para el sprint 4

---
+++ HotelCore.infraestructura.datos/Repositorio/RepositorioHabitacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelCore.dominio.entidades.Objetos;
+using HotelCore.infraestructura.datos.Modelo;
+
+public class RepositorioHabitacion : IRepositorioHabitacion
+{
+    private HotelDBEntities db = new HotelDBEntities();
+    public bool actualizar(Habitacion habitacion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool eliminar(int numero)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool insertar(Habitacion habitacion)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Habitacion obtenerHabitacion(int numero)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Habitacion> obtenerHabitaciones()
+    {
+        List<Habitacion> habitaciones = new List<Habitacion>();
+        habitaciones = db.Habitacion.ToList();
+        return habitaciones;
+    }
+}
