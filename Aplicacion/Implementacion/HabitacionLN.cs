c0b960828e2ba85859a7d7174eb09ba9ce736122
SRGAndrey <andreysrg17@gmail.com>
2018-06-14 17:25:03 -0600

    Arreglo de arquitectura

---
+++ Aplicacion/Implementacion/HabitacionLN.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelCore.dominio.entidades.Objetos;
+
+public class HabitacionLN : IHabitacionLN
+{
+    private IRepositorioHabitacion dominio;
+
+    public HabitacionLN(IRepositorioHabitacion repositorio)
+    {
+        dominio = repositorio;
+    }
+    public bool actualizar(Habitacion habitacion)
+    {
+        bool retorno;
+        retorno = dominio.actualizar(habitacion);
+        return retorno;
+    }
+
+    public bool eliminar(int numero)
+    {
+        bool retorno;
+        retorno = dominio.eliminar(numero);
+        return retorno;
+    }
+
+    public bool insertar(Habitacion habitacion)
+    {
+        bool retorno;
+        retorno = dominio.insertar(habitacion);
+        return retorno;
+    }
+
+    public Habitacion obtenerHabitacion(int numero)
+    {
+        Habitacion retornoHabitacion;
+        retornoHabitacion = dominio.obtenerHabitacion(numero);
+        return retornoHabitacion;
+    }
+
+    public List<Habitacion> obtenerHabitaciones()
+    {
+        List<Habitacion> retornoHabitaciones;
+        retornoHabitaciones = dominio.obtenerHabitaciones();
+        return retornoHabitaciones;
+    }
+
+    public List<AdministrarHabitacion> obtenerTodas()
+    {
+        List<AdministrarHabitacion> retornoHabitaciones;
+        retornoHabitaciones = dominio.obtenerTodas();
+        return retornoHabitaciones;
+    }
+
+    public Tipo_Habitacion obtenerTipoHabitacion(string tipo)
+    {
+        Tipo_Habitacion tipoHabitacion;
+        tipoHabitacion = dominio.obtenerTipoHabitacion(tipo);
+        return tipoHabitacion;
+    }
+
+    public Tipo_Habitacion actualizarTipo(string tipo, string descripcion, double tarifa)
+    {
+        Tipo_Habitacion tipoHabitacion;
+        tipoHabitacion = dominio.actualizarTipo(tipo, descripcion, tarifa);
+        return tipoHabitacion;
+    }
+
+    public List<HabitacionesDisponibles> obtenerHabitaciones(System.DateTime fechaInicio, System.DateTime fechaFinal, string tipos)
+    {
+        List<HabitacionesDisponibles> habitaciones = new List<HabitacionesDisponibles>();
+        habitaciones = dominio.obtenerHabitaciones(fechaInicio, fechaFinal, tipos);
+        return habitaciones;
+    }
+}
+
---
+++ HotelCore.infraestructuraIoC/FabricaIoC.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using HotelCore.dominio;
+using HotelCore.infraestructura.datos.Repositorio;
+
+public class FabricaIoC
+{
+    private static FabricaIoC _contenedor = new FabricaIoC();
+    private IUnityContainer _unityContainer;
+    private FabricaIoC()
+    {
+        _unityContainer = new UnityContainer();
+
+        //Inicializar las clases que van a base datos
+        _unityContainer.RegisterType<IRepositorioAdministrador, RepositorioAdministrador>();
+        _unityContainer.RegisterType<IRepositorioCliente, RepositorioCliente>();
+        _unityContainer.RegisterType<IRepositorioFacilidad, RepositorioFacilidad>();
+        _unityContainer.RegisterType<IRepositorioHabitacion, RepositorioHabitacion>();
+        _unityContainer.RegisterType<IRepositorioHotel, RepositorioHotel>();
+        _unityContainer.RegisterType<IRepositorioImagen, RepositorioImagen>();
+        _unityContainer.RegisterType<IRespositorioReservacion, RepositorioReservacion>();
+    }
+
+    public static FabricaIoC Contenedor
+    {
+        get
+        {
+            return _contenedor;
+        }
+    }
+
+    /// <summary>
+    ///   Crear una instancia de un objeto que implemente un tipo TServicio.
+    /// </summary>
+    /// <typeparam name = "TServicio">Tipo de servicio que deseamos resolver</typeparam>
+    /// <returns></returns>
+    public TServicio Resolver<TServicio>() where TServicio : class
+    {
+        return _unityContainer.Resolve<TServicio>();
+    }
+
+}
+
